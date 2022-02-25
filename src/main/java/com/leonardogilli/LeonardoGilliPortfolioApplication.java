9456e99d33b5c28b0abee46d55aeffa078f558dc
Leo <leonardogilli@outlook.com>
2022-02-25 14:52:51 -0300

    Primer commit configuracion pom

---
+++ src/main/java/com/leonardogilli/LeonardoGilliPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.leonardogilli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeonardoGilliPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeonardoGilliPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardogilli/LeonardoGilliPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardogilli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeonardoGilliPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
